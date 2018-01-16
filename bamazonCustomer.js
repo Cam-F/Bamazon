cdfd2723c63d48cd49498b562873aca134fd7478
cameron fuller <cameronmfuller@yahoo.com>
2018-01-16 04:22:36 -0500

    costumer functionality complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,16 +27,96 @@ function displayProducts() {
 
         console.table(res);
     });
+    bamazon();
 };
 
 // Prompt two messages
-//      1. ID of the item you would like to buy
-//      2. How many would you like to buy
+function bamazon() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            //      1. ID of the item you would like to buy
+            {
+                name: "selection",
+                type: "input",
+                message: "What is the ID# of the item you would like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    console.log("\n Sorry, you must enter an ID number first.");
+                    return false;
+                }
+            },
+            //      2. How many would you like to buy
+            {
+                name: "sQuantity",
+                type: "input",
+                message: "How many would you like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    console.log("\n Sorry, you must enter a number.");
+                    return false;
+                }
+            }
+        ]).then(function (answer) {
 
-// If not enough quantity display proper error handling
+            // Assigning the selected item to a var
 
-// Fulfill the order
-//      1. update the SQL to reflect remaining quanity
-//      2. Show the total price of the order
+            var selected;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].id === parseInt(answer.selection)) {
+                    selected = res[i];
+                }
+            }
 
-// 
+            // Comparing the selected quantity to our inventory
+            // Fulfill the order
+            //      1. update the SQL to reflect remaining quanity
+            //      2. Show the total price of the order
+
+            var inventory = parseInt(selected.quantity - answer.sQuantity);
+            if (inventory >= answer.sQuantity) {
+                var total = parseFloat(answer.sQuantity * selected.price);
+                console.log("You have successfully purchased " + answer.sQuantity + " " + selected.item_name + "'s for a total of $" + total);
+
+                reset();
+
+                // Updating the DB
+
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [{
+                        quantity: inventory
+                    },
+                    {
+                        id: selected.id
+                    }
+                    ]);
+
+                // Proper error handling
+
+            } else {
+                console.log("Sorry, we currently only have " + selected.quantity + " in stock.");
+
+                reset();
+            }
+        })
+    })
+}
+// Function to reset and display
+function reset() {
+    inquirer.prompt({
+        name: "reset",
+        type: "rawlist",
+        message: "Would you like to continue shopping?",
+        choices: ["YES", "NO"]
+    }).then(function(answer){
+        if (answer.reset === "YES"){
+            displayProducts();
+        } else {
+            connection.end();
+        }
+    })
+}
